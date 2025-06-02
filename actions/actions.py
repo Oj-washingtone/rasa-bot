2ea554c8797afa5f42c90ec152f4be97bc29936d
Oj-washingtone <jalangowashingtone@gmail.com>
2025-06-02 19:44:02 +0300

    Refactor action definitions and update domain responses for service provider interactions

--- actions/actions.py
+++ actions/actions.py
@@ -6,25 +6,6 @@ from rasa_sdk.events import SlotSet
 from rasa_sdk.executor import CollectingDispatcher
 
 
-# class ActionCheckSufficientFunds(Action):
-#     def name(self) -> Text:
-#         return "action_check_sufficient_funds"
-
-#     def run(
-#         self,
-#         dispatcher: CollectingDispatcher,
-#         tracker: Tracker,
-#         domain: Dict[Text, Any],
-#     ) -> List[Dict[Text, Any]]:
-#         # hard-coded balance for tutorial purposes. in production this
-#         # would be retrieved from a database or an API
-#         balance = 1000
-#         transfer_amount = tracker.get_slot("amount")
-#         has_sufficient_funds = transfer_amount <= balance
-#         return [SlotSet("has_sufficient_funds", has_sufficient_funds)]
-
-
-
 class ActionFetchServiceProviders(Action):
     def name(self) -> Text:
         return "action_fetch_service_providers"
